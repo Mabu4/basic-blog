d9c930802e01c1e2d4fae36d8c7620e25a0677c8
Maximilian Budziat <maximilian.budziat@scheer-group.com>
2023-03-09 13:32:53 +0100

    added smooth scroll and page for rights

---
+++ pages/rechtliches.tsx
@@ -0,0 +1,215 @@
+const Legal = () => {
+  return (
+    <main className="page-wrapper">
+      <div className="legal basic-container">
+        <h2 className="title-medium">Impressum</h2>
+        <div>
+          <h3 className="title-small">Angaben gemäß § 5 TMG:</h3>
+
+          <p>
+            Blue Dive <br /> Poststraße 32 <br /> 38440 Wolfsburg
+          </p>
+          <p>
+            Vertreten durch: <br /> Maximilian Budziat
+          </p>
+          <p>
+            Kontakt: <br /> E-Mail: max.budziat@gmx.de
+          </p>
+          <p>
+            Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: <br />
+            Maximilian Budziat <br /> Poststraße 32 <br /> 38440 Wolfsburg
+          </p>
+        </div>
+        <h2 className="title-medium" id="agb">
+          Allgemeine Geschäftsbedingungen (AGB)
+        </h2>
+        <div>
+          <h3 className="title-small">
+            1. Geltungsbereich und Vertragspartner
+          </h3>
+
+          <p>
+            Die nachfolgenden Allgemeinen Geschäftsbedingungen gelten für die
+            Nutzung des Blogs Blue Dive (im Folgenden Blog genannt) und für
+            sämtliche Dienstleistungen und Angebote, die im Rahmen des Blogs
+            erbracht werden. Betreiber des Blogs ist:
+          </p>
+          <p>
+            Maximilian Budziat Poststraße 32 38440 Wolfsburg E-Mail:
+            max.budziat@gmx.de
+          </p>
+          <h3 className="title-small">2. Nutzungsbedingungen</h3>
+          <p>
+            Die Nutzung des Blogs ist kostenlos und steht jedem Besucher offen.
+            Durch die Nutzung des Blogs akzeptiert der Besucher diese AGB sowie
+            die Datenschutzbestimmungen des Betreibers. Der Betreiber behält
+            sich das Recht vor, den Blog oder Teile davon jederzeit ohne
+            vorherige Ankündigung zu ändern oder zu entfernen.
+          </p>
+          <h3 className="title-small">3. Haftungsausschluss</h3>
+          <p>
+            Der Betreiber des Blogs übernimmt keine Gewähr für die Richtigkeit,
+            Vollständigkeit und Aktualität der Informationen, die auf dem Blog
+            zur Verfügung gestellt werden. Insbesondere können keine Garantien
+            für die Inhalte von verlinkten Websites Dritter übernommen werden.
+            Der Betreiber haftet nicht für Schäden, die aus der Nutzung des
+            Blogs oder der Inhalte entstehen können.
+          </p>
+          <h3 className="title-small">4. Affiliate-Links</h3>
+          <p>
+            In einigen Blogbeiträgen werden Affiliate-Links verwendet. Der
+            Betreiber des Blogs erhält möglicherweise eine Provision, wenn der
+            Besucher über den Affiliate-Link einen Kauf tätigt. Der Betreiber
+            übernimmt keine Haftung für die angebotenen Produkte und
+            Dienstleistungen sowie für die Verfügbarkeit der Links.
+          </p>
+          <h3 className="title-small">5. Urheberrechte</h3>
+          <p>
+            Alle Inhalte des Blogs, einschließlich Texte, Bilder und Videos,
+            unterliegen dem Urheberrecht und dürfen ohne ausdrückliche
+            Genehmigung des Betreibers nicht kopiert, vervielfältigt oder
+            anderweitig verwendet werden.
+          </p>
+          <h3 className="title-small">6. Datenschutz</h3>
+          <p>
+            Der Betreiber des Blogs erhebt und verarbeitet personenbezogene
+            Daten gemäß den geltenden Datenschutzbestimmungen. Weitere
+            Informationen zum Datenschutz finden Sie in der Datenschutzerklärung
+            des Betreibers.
+          </p>
+          <h3 className="title-small">7. Schlussbestimmungen</h3>
+          <p>
+            Es gilt das Recht der Bundesrepublik Deutschland. Sollte eine
+            Bestimmung dieser AGB unwirksam oder undurchführbar sein oder
+            werden, so wird die Wirksamkeit der übrigen Bestimmungen dadurch
+            nicht berührt. Anstelle der unwirksamen oder undurchführbaren
+            Bestimmung tritt eine Regelung, die dem wirtschaftlichen Zweck der
+            unwirksamen oder undurchführbaren Bestimmung am nächsten kommt.
+          </p>
+        </div>
+        <h2 className="title-medium" id="datenschutz">
+          Datenschutz
+        </h2>
+        <div>
+          <h3 className="title-small">1. Verantwortliche Stelle</h3>
+          <p>Verantwortliche Stelle im Sinne der Datenschutzgesetze ist:</p>
+          <p>
+            Maximilian Budziat <br />
+            Poststraße 32 <br />
+            38440 Wolfsburg <br />
+            E-Mail: max.budziat@gmx.de
+          </p>
+          <h3 className="title-small">2. Datenerhebung und -verarbeitung</h3>
+          <h3 className="title-small">2.1. Server-Logfiles</h3>
+          <p>
+            Bei jedem Zugriff auf den Blog werden Nutzungsdaten durch den
+            jeweiligen Internetbrowser übermittelt und in Protokolldateien, den
+            sogenannten Server-Logfiles, gespeichert. Die dabei gespeicherten
+            Daten umfassen insbesondere die folgenden Informationen:
+          </p>
+          <ul>
+            <li>IP-Adresse des zugreifenden Rechners</li>
+            <li>Datum und Uhrzeit des Zugriffs</li>
+            <li>Name und URL der abgerufenen Datei</li>
+            <li>Referrer-URL (die zuvor besuchte Seite)</li>
+            <li>
+              verwendeter Browser und ggf. das Betriebssystem des Rechners sowie
+              der Name des Access-Providers
+            </li>
+          </ul>
+          <p>
+            Diese Daten dienen der technischen Administration und der Sicherheit
+            des Systems und werden gegebenenfalls anonymisiert ausgewertet. Eine
+            Zusammenführung dieser Daten mit anderen Datenquellen wird nicht
+            vorgenommen.
+          </p>
+          <h3 className="title-small">2.2. Cookies</h3>
+          <p>
+            Der Blog verwendet Cookies, um die Benutzerfreundlichkeit zu erhöhen
+            und um bestimmte Funktionen zu ermöglichen. Cookies sind kleine
+            Textdateien, die auf Ihrem Endgerät gespeichert werden. Die meisten
+            der von uns verwendeten Cookies sind sogenannte Session-Cookies, die
+            nach Ende Ihres Besuchs automatisch gelöscht werden. Andere Cookies
+            bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen oder
+            die Speicherfrist abläuft. Diese Cookies ermöglichen es uns, Ihren
+            Browser beim nächsten Besuch wiederzuerkennen.
+          </p>
+          <p>
+            Sie können die Installation von Cookies durch eine entsprechende
+            Einstellung Ihrer Browser-Software verhindern. Bitte beachten Sie
+            jedoch, dass in diesem Fall gegebenenfalls nicht alle Funktionen des
+            Blogs vollumfänglich genutzt werden können.
+          </p>
+          <h3 className="title-small">2.3. Kontaktformular</h3>
+          <p>
+            Wenn Sie das Kontaktformular auf dem Blog nutzen, werden die von
+            Ihnen eingegebenen Daten zur Bearbeitung Ihrer Anfrage gespeichert.
+            Diese Daten werden nicht ohne Ihre Einwilligung an Dritte
+            weitergegeben.
+          </p>
+          <h3 className="title-small">2.4. Affiliate-Links</h3>
+          <p>
+            In einigen Blogbeiträgen werden Affiliate-Links verwendet. Wenn Sie
+            einen solchen Link anklicken und einen Kauf tätigen, erhält der
+            Betreiber des Blogs möglicherweise eine Provision. Der Betreiber
+            speichert jedoch keine personenbezogenen Daten im Zusammenhang mit
+            den Affiliate-Links.
+          </p>
+          <h3 className="title-small">3. Datensicherheit</h3>
+          <p>
+            Der Betreiber des Blogs setzt technische und organisatorische
+            Sicherheitsmaßnahmen ein, um die gespeicherten Daten gegen zufällige
+            oder vorsätzliche Manipulation, Verlust oder Zugriff unberechtigter
+            Personen zu schützen. Die Sicherheitsmaßnahmen werden entsprechend
+            der technologischen Entwicklung fortlaufend verbessert.
+          </p>
+          <h3 className="title-small">4. Betroffenenrechte</h3>
+          <p>
+            Sie haben das Recht, Auskunft über die von uns zu Ihrer Person
+            gespeicherten Daten zu erhalten. Bei Vorliegen der gesetzlichen
+            Voraussetzungen haben Sie zudem ein Recht auf Berichtigung, Sperrung
+            oder Löschung Ihrer personenbezogenen Daten sowie auf Einschränkung
+            der Verarbeitung und Widerspruch gegen die Verarbeitung. Sie haben
+            auch das Recht auf Datenübertragbarkeit, d.h. das Recht, Ihre Daten
+            in einem strukturierten, gängigen und maschinenlesbaren Format zu
+            erhalten und diese Daten einem anderen Verantwortlichen ohne
+            Behinderung durch uns zu übermitteln.
+          </p>
+          <p>
+            Soweit die Verarbeitung Ihrer personenbezogenen Daten aufgrund einer
+            Einwilligung erfolgt, haben Sie das Recht, diese Einwilligung
+            jederzeit zu widerrufen. Die Rechtmäßigkeit der Verarbeitung Ihrer
+            personenbezogenen Daten bis zum Widerruf wird davon nicht berührt.
+          </p>
+          <p>
+            Wenn Sie der Meinung sind, dass die Verarbeitung Ihrer
+            personenbezogenen Daten gegen die Datenschutzgrundverordnung
+            verstößt, haben Sie das Recht, eine Beschwerde bei einer
+            Aufsichtsbehörde einzulegen.
+          </p>
+          <h3 className="title-small">
+            5. Änderungen der Datenschutzerklärung
+          </h3>
+          <p>
+            Der Betreiber des Blogs behält sich vor, diese Datenschutzerklärung
+            zu ändern, um sie an geänderte Rechtslagen oder bei Änderungen des
+            Dienstes sowie der Datenverarbeitung anzupassen. Die neue
+            Datenschutzerklärung wird dann auf der Website veröffentlicht.
+          </p>
+          <h3 className="title-small">6. Kontaktaufnahme</h3>
+          <p>
+            Wenn Sie Fragen zur Verarbeitung Ihrer personenbezogenen Daten haben
+            oder Ihre Betroffenenrechte ausüben möchten, können Sie sich
+            jederzeit an die verantwortliche Stelle wenden:
+          </p>
+          <p>
+            Maximilian Budziat <br /> Poststraße 32 <br /> 38440 Wolfsburg
+            <br /> E-Mail: max.budziat@gmx.de
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Legal;
